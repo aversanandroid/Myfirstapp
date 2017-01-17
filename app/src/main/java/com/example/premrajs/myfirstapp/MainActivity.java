9a2b1411195f8184af33e5e4266960890275cbc7
aversanandroid <premrajs@aversan.com>
2017-01-17 14:44:12 +0530

    commit

--- app/src/main/java/com/example/premrajs/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/premrajs/myfirstapp/MainActivity.java
@@ -29,21 +29,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void addNotification() {
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.abc)
-                        .setContentTitle("Notifications Example")
-                        .setContentText("This is a test notification");
 
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
-    }
-}
 }
