e98211961a0751f6cbbe096be1f2701451940da7
aversanandroid <premrajs@aversan.com>
2017-01-17 12:55:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/premrajs/myfirstapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.premrajs.myfirstapp;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample);
+        Button button = (Button) findViewById(R.id.button);
+        final EditText Etext= (EditText) findViewById(R.id.textbox);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               if(Etext.getText().toString().equalsIgnoreCase("Prem"))
+                {
+                    setContentView(R.layout.activity_main);
+                }
+            }
+        });
+
+
+    }
+
+    private void addNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.abc)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+}
+}
